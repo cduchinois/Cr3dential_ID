537376dc336c31d4eec7430bdd3e0f265fcf9988
yue wang <yuewang@macbook-air-1.home>
2024-11-05 14:29:18 +0100

    web3auth updated

--- Front-End/src/lib/web3Auth.ts
+++ Front-End/src/lib/web3Auth.ts
@@ -3,10 +3,7 @@ import { Web3Auth } from '@web3auth/modal';
 import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
 import { XrplPrivateKeyProvider } from '@web3auth/xrpl-provider';
 
-//BD7NISfuA-_b-HCiIwKYM5XmBvE4c0LJrq_FY3X1iu3mk_rq082-jlF3hgbRpypEKyFvlJ15DGNEYDlTF4fgLOo
-const clientId =
-  process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID;
-
+const clientId = process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID || '';
 
 export const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.XRPL,
@@ -65,25 +62,14 @@ export const xrplProvider = new XrplPrivateKeyProvider({
 });
 
 export const instantiateWeb3Auth = () => {
+  if (!process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID) {
+    throw new Error('NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID is not defined in environment variables');
+  }
+  
   return new Web3Auth({
-    clientId,
-    // uiConfig refers to the whitelabeling options, which is available only on Growth Plan and above
-    // Please remove this parameter if you're on the Base Plan
-    uiConfig: {
-      appName: 'Cr3dential',
-      // appLogo: "https://web3auth.io/images/web3authlog.png", // Your App Logo Here
-      theme: {
-        primary: 'red',
-      },
-      mode: 'dark',
-      logoLight: 'https://web3auth.io/images/web3authlog.png',
-      logoDark: 'https://web3auth.io/images/web3authlogodark.png',
-      defaultLanguage: 'en', // en, de, ja, ko, zh, es, fr, pt, nl
-      loginGridCol: 3,
-      primaryButton: 'externalLogin', // "externalLogin" | "socialLogin" | "emailLogin"
-      uxMode: UX_MODE.REDIRECT,
-    },
+    clientId: process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID,
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
     privateKeyProvider: xrplProvider,
+    chainConfig: chainConfig
   });
 };
