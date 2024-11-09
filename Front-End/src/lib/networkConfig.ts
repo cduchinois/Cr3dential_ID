0c1c76d5de0df0b4d658b33a23e600312f5222d4
Soona <code@chainsona.dev>
2024-11-09 17:22:23 +0100

    feat: set xrpl network

--- Front-End/src/lib/networkConfig.ts
+++ Front-End/src/lib/networkConfig.ts
@@ -1,12 +1,13 @@
-export type NetworkType = 'mainnet' | 'testnet' | 'devnet';
+export type NetworkType = "mainnet" | "testnet" | "devnet";
 
 export const NETWORK_URLS = {
-  mainnet: 'wss://s1.ripple.com/',
-  testnet: 'wss://s.altnet.rippletest.net:51233',
-  devnet: 'wss://s.devnet.rippletest.net:51233'
+  mainnet: "wss://s1.ripple.com/",
+  testnet: "wss://s.altnet.rippletest.net:51233",
+  devnet: "wss://s.devnet.rippletest.net:51233",
 };
 
-export const DEFAULT_NETWORK: NetworkType = 'testnet';
+export const DEFAULT_NETWORK: NetworkType =
+  (process.env.XRPL_NETWORK as NetworkType) || "testnet";
 
 // Store current network in memory
 let currentNetwork: NetworkType = DEFAULT_NETWORK;
@@ -16,6 +17,8 @@ export const setCurrentNetwork = (network: NetworkType) => {
   currentNetwork = network;
 };
 
-export const getNetworkUrl = (network: NetworkType = currentNetwork): string => {
+export const getNetworkUrl = (
+  network: NetworkType = currentNetwork
+): string => {
   return NETWORK_URLS[network];
-}; 
+};
